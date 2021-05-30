f27e6a055afca85ad960859ff88963e62021b105
manoharr180 <61632610+manoharr180@users.noreply.github.com>
2021-05-30 13:25:19 +0530

    added docker and docker compose files

--- src/startup.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from pyRestAPI.src.router import routers
-
-app = Flask(__name__)
-
-routers.init_route(app)
-
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True, threaded=False)
